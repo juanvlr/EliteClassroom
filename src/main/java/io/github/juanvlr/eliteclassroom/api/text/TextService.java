f3a9ac7ea2764acc411d325dfefd0eece9688257
Juan Valero <98854476+juanvlr@users.noreply.github.com>
2022-03-01 22:55:31 +0100

    Add methods to send text to a bukkit online player

--- src/main/java/io/github/juanvlr/eliteclassroom/api/text/TextService.java
+++ src/main/java/io/github/juanvlr/eliteclassroom/api/text/TextService.java
@@ -1,5 +1,6 @@
 package io.github.juanvlr.eliteclassroom.api.text;
 
+import co.aikar.commands.bukkit.contexts.OnlinePlayer;
 import io.github.juanvlr.eliteclassroom.api.text.component.TranslatableComponent;
 import io.github.juanvlr.eliteclassroom.api.text.title.TranslatableTitle;
 import net.kyori.adventure.audience.Audience;
@@ -11,7 +12,15 @@ public interface TextService {
 
     void sendMessage(Player player, TranslatableComponent message);
 
+    default void sendMessage(OnlinePlayer player, TranslatableComponent message) {
+        this.sendMessage(player.getPlayer(), message);
+    }
+
     void sendTitle(Audience audience, TranslatableTitle title);
 
     void sendTitle(Player player, TranslatableTitle title);
+
+    default void sendTitle(OnlinePlayer player, TranslatableTitle title) {
+        this.sendTitle(player.getPlayer(), title);
+    }
 }
