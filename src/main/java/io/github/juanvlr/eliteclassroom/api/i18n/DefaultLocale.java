527b83c78d5f043dafe90ce202085dcdda670e50
Juan Valero <98854476+juanvlr@users.noreply.github.com>
2022-03-03 00:55:25 +0100

    Add an annotation for the default locale

---
+++ src/main/java/io/github/juanvlr/eliteclassroom/api/i18n/DefaultLocale.java
@@ -0,0 +1,11 @@
+package io.github.juanvlr.eliteclassroom.api.i18n;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DefaultLocale {
+
+}
--- src/main/java/io/github/juanvlr/eliteclassroom/api/i18n/InternalizationServiceImpl.java
+++ src/main/java/io/github/juanvlr/eliteclassroom/api/i18n/InternalizationServiceImpl.java
@@ -2,12 +2,20 @@ package io.github.juanvlr.eliteclassroom.api.i18n;
 
 import net.kyori.adventure.audience.Audience;
 
+import javax.inject.Inject;
 import java.util.Locale;
 
 public class InternalizationServiceImpl implements InternalizationService {
 
+    private final Locale defaultLocale;
+
+    @Inject
+    public InternalizationServiceImpl(@DefaultLocale Locale defaultLocale) {
+        this.defaultLocale = defaultLocale;
+    }
+
     @Override
     public Locale getLocale(Audience audience) {
-        return Locale.FRENCH;
+        return this.defaultLocale;
     }
 }
