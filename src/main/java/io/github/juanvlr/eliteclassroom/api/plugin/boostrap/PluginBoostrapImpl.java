ce843e4aa92c965a357bece827686e59b8cd233f
Juan Valero <98854476+juanvlr@users.noreply.github.com>
2022-03-02 23:14:32 +0100

    Create the plugin configuration if it doesn't exist on plugin boostrap

--- src/main/java/io/github/juanvlr/eliteclassroom/api/plugin/boostrap/PluginBoostrapImpl.java
+++ src/main/java/io/github/juanvlr/eliteclassroom/api/plugin/boostrap/PluginBoostrapImpl.java
@@ -3,22 +3,26 @@ package io.github.juanvlr.eliteclassroom.api.plugin.boostrap;
 import io.github.juanvlr.eliteclassroom.api.command.CommandRegisterer;
 import io.github.juanvlr.eliteclassroom.api.listener.ListenerRegisterer;
 import io.github.juanvlr.eliteclassroom.api.plugin.PluginDataFolder;
+import org.bukkit.plugin.Plugin;
 
 import javax.inject.Inject;
 import java.io.File;
 
 public class PluginBoostrapImpl implements PluginBootstrap {
 
+    private final Plugin plugin;
     private final File dataFolder;
     private final CommandRegisterer commandRegisterer;
     private final ListenerRegisterer listenerRegisterer;
 
     @Inject
     public PluginBoostrapImpl(
+            Plugin plugin,
             @PluginDataFolder File dataFolder,
             CommandRegisterer commandRegisterer,
             ListenerRegisterer listenerRegisterer
     ) {
+        this.plugin = plugin;
         this.dataFolder = dataFolder;
         this.commandRegisterer = commandRegisterer;
         this.listenerRegisterer = listenerRegisterer;
@@ -31,6 +35,8 @@ public class PluginBoostrapImpl implements PluginBootstrap {
                 throw new Exception("An exception occurred while attempting to create the data folder");
             }
         }
+
+        this.plugin.saveDefaultConfig();
 
         this.commandRegisterer.registerCommands();
         this.listenerRegisterer.registerListeners();
